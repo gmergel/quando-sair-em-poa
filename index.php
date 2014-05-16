78ccc947af5b0e68d3bc07208ae8408e86556cfc
Germano Mergel <gmergel@gmail.com>
2014-05-16 14:28:25 -0300

    aaa

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 <html>
 	<head>
 		<title>Visu</title>
+		<style>
+			body{margin:0;padding:0;background-color: #ccc;font-family: Helvetica, Arial;}
+			h1{color: #555;text-align: center;}
+			#container{width: 1000px;height: 680px;background-color: white;margin: 60px auto auto auto;border-radius: 5px;box-shadow: 0px 6px 13px 0px #999;padding: 1px 15px;}
+		</style>
 	</head>
 	<body>
-		<div></div>
+		<div id="container">
+			<h1>Quando sair em Porto Alegre?</h1>
+		</div>
 	</body>
 </html>
